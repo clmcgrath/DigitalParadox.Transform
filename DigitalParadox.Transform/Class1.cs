cf03c5fc6505b3e1ff1f1e11c8ee6accc4e282c0
Chris McGrath <clmcgrathdev@gmail.com>
2017-10-31 14:08:24 -0400

    initial commit

---
+++ DigitalParadox.Transform/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Nuke.Core;
+using System;
+using System.IO;
+
+namespace DigitalParadox.SlowCheetah
+{
+    public class TransformJDT : NukeBuild {
+        
+    }
+    public class TransformXDT : NukeBuild
+    {
+        public FileInfo TransformPath { get; set; }
+        public FileInfo TransformTargetPath { get; set; }
+
+        protected string Description { get; set; } = "Apply XDT Transform to target file"
+
+        Target Transform_XML => _ => _
+            .Requires(() => TransformPath)
+            .Requires(() => TransformTargetPath )
+            .Description("Apply XDT Transform to target file")
+            .Executes(() => {
+                XDTTransform 
+                    
+            });
+    }
+
+
+}
